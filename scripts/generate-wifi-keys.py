d3b2fdb0b790ce2d473c8b6deba4f4c6d23702ef
Peter Law <PeterJCLaw@gmail.com>
2019-10-15 21:55:45 +0100

    Add a script to help generate the robot wifi keys

---
+++ scripts/generate-wifi-keys.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import random
+import string
+import sys
+
+LENGTH = 10
+CHARACTERS = (string.ascii_letters + string.digits) * 5 + string.punctuation.replace('"', '')
+
+print("Input TLAs", file=sys.stderr)
+
+for line in sys.stdin.readlines():
+    tla = line.strip().upper()
+    if not tla:
+        break
+    password = "".join(random.choice(CHARACTERS) for _ in range(LENGTH))
+    print('{}: "{}"'.format(tla, password))
