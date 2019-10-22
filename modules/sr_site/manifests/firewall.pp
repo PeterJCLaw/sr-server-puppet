50220775503c58cc58f6614dde340fade26af724
Peter Law <PeterJCLaw@gmail.com>
2019-10-22 22:00:46 +0100

    Push firewall package config into the module
    Rather than doing this ourselves.

--- modules/sr_site/manifests/firewall.pp
+++ modules/sr_site/manifests/firewall.pp
@@ -1,30 +1,16 @@
 # A wall of fire!
 
 class sr_site::firewall {
-
-  # Install standard firewall arrangement, iptables, and disable firewalld
-  # as it isn't useful for us here.
-  service { 'firewalld':
-    ensure => stopped,
-    enable => false,
-  }
-
-  package { 'iptables-services':
-    ensure => latest,
-  }
-
-  service { 'iptables':
-    ensure => running,
-    enable => true,
-    require => [Service['firewalld'], Package['iptables-services']],
-  }
-
   # Purge unmanaged firewall resources
   #
   # This will clear any existing rules, and make sure that only rules
   # defined in puppet exist on the machine
   resources { 'firewall':
     purge => true,
+  }
+
+  class { 'firewall':
+    ensure => running,
   }
 
   class { 'sr_site::fw_pre':
