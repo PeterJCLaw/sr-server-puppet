0051dd9f3b0641f76c2b7daf98cb6138b11ef9bf
Peter Law <PeterJCLaw@gmail.com>
2019-10-26 11:34:51 +0100

    Use known-safe punctuation in robot WiFi passwords

--- scripts/generate-wifi-keys.py
+++ scripts/generate-wifi-keys.py
@@ -4,8 +4,11 @@ import random
 import string
 import sys
 
+# Characters which are easy to type and relatively safe within quoted YAML
+LIMITED_PUNCTUATION = "!£$%^&*-=_+@'~.,/<>[]{}()"
+
 LENGTH = 10
-CHARACTERS = (string.ascii_letters + string.digits) * 5 + string.punctuation.replace('"', '')
+CHARACTERS = (string.ascii_letters + string.digits) * 5 + LIMITED_PUNCTUATION
 
 print("Input TLAs (then press Ctrl+D for end)", file=sys.stderr)
 
